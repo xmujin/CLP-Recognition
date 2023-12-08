edb975a1f77180b205b949c1ee447cb11f34e69a
xiangxun <806181859@qq.com>
2023-12-08 09:53:45 +0800

    更新了一些内容

---
+++ src/main.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# @Time    : 日期
+# @Author  : 作者
+# @Sid     : 学号
+# @File    : 当前文件名
+# @Description : 该程序的功能
+
+print(1) //现在是一个开始
+
+def fun(a, b):
+    """
+    该函数的描述
+    :param a: 参数说明
+    :param b: 参数说明
+    """
+    print("sdf")
+
+
+
+

d6bb60ab01f206473d01e6e78802fa42083ced35
xiangxun <806181859@qq.com>
2023-12-19 10:42:15 +0800

    test

--- src/plate_right.py
+++ src/plate_right.py
@@ -1,5 +1,13 @@
-import os
+# -*- coding: utf-8 -*-
+# @Time    : 2023-12-14
+# @Author  : 向波
+# @Sid     : 12103990437
+# @File    : character.py
+# @Description : 用于字符分割，使用投影法，并将分割好的图片交给另一个程序进行处理
 
+
+
+import os
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt

b7d7b89b27db1e3a556e64928b9613b82f0bd64b
xiangxun <806181859@qq.com>
2023-12-22 00:06:10 +0800

    哈哈

--- src/char_recognition.py
+++ src/char_recognition.py
@@ -11,7 +11,7 @@ import torchvision
 
 from torchvision.transforms import functional
 from src.build_char_recognition_model import CharCNN
-
+# 67= 10 + 26 + 31     0,9  10, 35    36, 66
 dataset = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
            'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T',
            'U', 'V', 'W', 'X', 'Y', 'Z', '川', '鄂', '赣', '甘', '贵', '桂', '黑', '沪',
@@ -35,36 +35,6 @@ def GetCharModel(modelPath):
     model.eval()
     return model
 
-
-# 读取图片
-# img = cv2.imread("../images/cnn_char_train/0/4-3.jpg", cv2.IMREAD_GRAYSCALE)
-# img = functional.to_tensor(img).unsqueeze(0).to(device)
-
-# def GetChar(model, imgSrc):
-#     """
-#     识别字符
-#     :param model: 使用的模型
-#     :param imgSrc: 图片
-#     :return: 识别的字符
-#     """
-#     print("start")
-#     device = torch.device("cuda")
-#     print("fnish1")
-#     # 将原图像改为训练时用到的尺寸
-#     img = cv2.resize(imgSrc, (20, 20))
-#     print("fnish2")
-#     img = functional.to_tensor(img).unsqueeze(0).to(device)
-#     print("fnish3")
-#     with torch.no_grad():
-#         print("fnish3--")
-#         prediction = model(img)
-#     print("fnish4")
-#     preClass = torch.argmax(prediction)
-#     print("end")
-#     return dataset[preClass.item()]
-#
-
-
 class CharRecModel:
     def __init__(self, modelPath):
         self.model = None
@@ -80,15 +50,32 @@ class CharRecModel:
         self.model.eval()
         # 进行第一次预先推理，以成功加载模型
         arr = np.zeros((20, 20), dtype=np.float32)
-        self.GetChar(arr)
-    def GetChar(self, imgSrc):
+        self.GetChar(arr, 0)
+    def GetChar(self, imgSrc, index):
+        kernel = np.ones((3, 3), np.uint8)
+        img = cv2.erode(imgSrc, kernel, iterations=1)
+        # cv2.imshow("sdf", img)
         device = torch.device("cuda")
         # 将原图像改为训练时用到的尺寸
-        img = cv2.resize(imgSrc, (20, 20))
+        # img = cv2.resize(imgSrc, (20, 20))
+        img = cv2.resize(img, (20, 20))
         img = functional.to_tensor(img).unsqueeze(0).to(device)
         # 进行推理
         with torch.no_grad():
             prediction = self.model(img)
-        preClass = torch.argmax(prediction)
-        return dataset[preClass.item()]
+
+        # preClass = torch.argmax(prediction)
+        charIdx = 0
+        arr = prediction.cpu().numpy()[0]
+        if(index == 0): #
+            arr = arr[36:67]
+            charIdx = np.argmax(arr) + 36
+            charIdx = 58
+        elif index == 1:
+            arr = arr[10:36]
+            charIdx = np.argmax(arr) + 10
+        else:
+            arr = arr[:36]
+            charIdx = np.argmax(arr)
+        return dataset[charIdx]
 
--- src/ui/my_mainwindow.py
+++ src/ui/my_mainwindow.py
@@ -3,18 +3,19 @@ import sys
 import cv2
 import numpy as np
 from PyQt5 import QtCore
-from PyQt5.QtCore import QDir, QStringListModel, QModelIndex, QThread, pyqtSignal
+from PyQt5.QtCore import QDir, QStringListModel, QModelIndex, QThread, pyqtSignal, QItemSelectionModel
 from PyQt5.QtGui import QPixmap, QImage
 from PyQt5.QtWidgets import QMainWindow, QApplication, QFileDialog
-
 from src.ui.my_ui_mainwindow import Ui_MainWindow
 from src.show_label import GetVertexes
 from src.plate_right import GetRightPlate
-from src.character_split import FindVSplitPos, FindHSplitPos, SplitCharacters
+from src.character_split import FindVSplitPos, FindHSplitPos, SplitCharacters, SplitPosPlate
 from src.char_recognition import GetCharModel, CharRecModel
+from src.searchPlate import getPicture
 
 class ModelLoaderThread(QThread):
     modelSig = pyqtSignal(object)
+
     def run(self):
         self.modelSig.emit(CharRecModel("model/plate_char_model.pth"))
         print("模型加载完成")
@@ -22,20 +23,33 @@ class ModelLoaderThread(QThread):
 
 class MyMainWindow(QMainWindow, Ui_MainWindow):
     def __init__(self, parent=None):
-        super(MyMainWindow, self).__init__(parent)# parent确定父组件和子组件的关系，与继承无关
+        super(MyMainWindow, self).__init__(parent)  # parent确定父组件和子组件的关系，与继承无关
         self.setupUi(self)  # 继承 Ui_MainWindow 界面类
-        #加载字符检测模型
+        # 加载字符检测模型
         # self.char_rec_mode = CharRecModel("model/plate_char_model.pth")
         self.char_rec_mode = None
         self.model_loader_thread = ModelLoaderThread()
         self.model_loader_thread.modelSig.connect(self.GetLoadedModel)
         # 启动模型加载线程
         self.model_loader_thread.start()
-        self.labels = [self.char_a, self.char_b,self.char_c,
-                       self.char_d,self.char_e,self.char_f, self.char_g,]
+        self.labels = [self.char_a, self.char_b, self.char_c,
+                       self.char_d, self.char_e, self.char_f, self.char_g]
+        self.reses = [self.res_a, self.res_b, self.res_c,
+                      self.res_d, self.res_e, self.res_f, self.res_g]
+        # 定义菜单选择项
+        self.selectedMenuIdx = 0 # 默认是一类车牌检测
+        self.action1.triggered.connect(self.ChangeMenuIdx0)
+        self.action2.triggered.connect(self.ChangeMenuIdx1)
+
+
+    def ChangeMenuIdx0(self):
+        self.selectedMenuIdx = 0
+    def ChangeMenuIdx1(self):
+        self.selectedMenuIdx = 1
 
     def GetLoadedModel(self, model):
         self.char_rec_mode = model
+
     def showFileDialog(self):
         # 显示文件夹选择对话框
         path = QFileDialog.getExistingDirectory(self, '选择文件夹', '.', QFileDialog.ShowDirsOnly)
@@ -45,47 +59,51 @@ class MyMainWindow(QMainWindow, Ui_MainWindow):
         model = QStringListModel()
         model.setStringList(fileList)
         self.listView.setModel(model)
+
     def showPicture(self, index: QModelIndex):
         self.fileName = index.data()
-        #print(self.fileName)
+        # print(self.fileName)
         self.filePath = QDir(self.lineEdit.text()).filePath(self.fileName)
         # 显示图片到 QLabel 上
         # pixmap = QPixmap(file_path)
         pixmap = QPixmap(self.filePath)
-        #self.picture.setScaledContents(True)
+        # self.picture.setScaledContents(True)
         self.picture.setPixmap(pixmap)
         # self.picture.set
-        #self.picture.setAlignment(QtCore.Qt.AlignmentFlag.AlignCenter)
+        # self.picture.setAlignment(QtCore.Qt.AlignmentFlag.AlignCenter)
         # 获取 QLabel 的大小
         label_size = self.picture.size()
-        #print(label_size)
+        # print(label_size)
         # 调整图片大小以适应 QLabel
         resized_pixmap = pixmap.scaled(label_size, QtCore.Qt.AspectRatioMode.KeepAspectRatio)
         # 显示调整大小后的图片到 QLabel 上
         self.picture.setPixmap(resized_pixmap)
-        #file_name = self.listView.model().data(index)
-        #print(file_name)
+        # file_name = self.listView.model().data(index)
+        # print(file_name)
         # file_list = self.getFileList(path)
         # self.listView.setStringList(file_list)
+
     # def getFileList(self, path):
     #     # 使用 QDir 获取文件夹下的文件列表
     #     dir_model = QDir(path)
     #     file_list = [file_info.fileName() for file_info in dir_model.entryInfoList() if file_info.isFile()]
     #     return file_list
     def cv2BgrToPixmap(self, img, size):
-        cur = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
+        #cur = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        cur = img.copy()
         # todo 图片处理
-        _, cur = cv2.threshold(cur, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
-        #
-        height, width = cur.shape
-        bytes_per_line = 1 * width
-        qt_image = QImage(cur.data, width, height, bytes_per_line, QImage.Format_Grayscale8)
+        # _, cur = cv2.threshold(cur, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+        height, width, channel = cur.shape
+        bytes_per_line = channel * width
+        #qt_image = QImage(cur.data, width, height, bytes_per_line, QImage.Format_Grayscale8)
+        qt_image = QImage(cur.data, width, height, bytes_per_line, QImage.Format_BGR888)
         pixmap = QPixmap.fromImage(qt_image)
         pixmap = pixmap.scaled(size, QtCore.Qt.AspectRatioMode.KeepAspectRatio)
         return pixmap
+
     def cv2BinaryToPixmap(self, cur, size):
         cur = cv2.cvtColor(cur, cv2.COLOR_GRAY2RGB)
+
         height, width, h = cur.shape
         bytes_per_line = h * width
         qt_image = QImage(cur.data, width, height, bytes_per_line, QImage.Format_RGB888)
@@ -94,25 +112,40 @@ class MyMainWindow(QMainWindow, Ui_MainWindow):
         return pixmap
 
     def showRecognition(self):
+        self.showRecognition1()
+    def showRecognition1(self):
         img = cv2.imread(self.filePath)
-        # 该车牌
-        img2 = GetRightPlate(img, self.fileName)
-        # nextImg，单通道灰度图
-        nextImg = img2.copy()
-        nextImg = cv2.cvtColor(nextImg, cv2.COLOR_BGR2GRAY)
-        _, nextImg = cv2.threshold(nextImg, 127, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
-        #print(nextImg)
-        # splitPos =
+        imgHaveLine = img.copy()
+        if self.selectedMenuIdx == 0:
+            # 该车牌为矫正后的车牌, 为BGR图
+            imgRight = GetRightPlate(img, self.fileName)
+        else:
+            # 对于二类识别，车牌图像不应由文件名给出
+            imgRight = getPicture(img)
+        # charIm 用于分割的图，需要转为二值图，从已经矫正的车牌图的副本
+        charImg = imgRight.copy()
+        charImg = cv2.cvtColor(charImg, cv2.COLOR_BGR2GRAY)
+        _, charImg = cv2.threshold(charImg, 127, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
 
-        characters = SplitCharacters(nextImg)
-        print(len(characters))
-        vertexs = GetVertexes(self.fileName)
-        vertexsT = np.array([vertexs[0], vertexs[1], vertexs[3], vertexs[2]], dtype=int)
-        cv2.polylines(img, [vertexsT], isClosed=True, color=(0, 255, 0), thickness=2)
-        self.picture.setPixmap(self.cv2BgrToPixmap(img, self.picture.size()))
-        self.platePos.setPixmap(self.cv2BgrToPixmap(img2, self.platePos.size()))
+        # 具有分割位置的图,需要进行处理
+        splitPlateImg = SplitPosPlate(imgRight)
+
+        characters = SplitCharacters(charImg)
+
+        if self.selectedMenuIdx == 0:
+            vertexs = GetVertexes(self.fileName)
+            vertexsT = np.array([vertexs[0], vertexs[1], vertexs[3], vertexs[2]], dtype=int)
+            cv2.polylines(imgHaveLine, [vertexsT], isClosed=True, color=(0, 255, 0), thickness=2)
+            self.picture.setPixmap(self.cv2BgrToPixmap(imgHaveLine, self.picture.size()))
+
+        # 显示只有车牌的图片
+        self.platePos.setPixmap(self.cv2BgrToPixmap(imgRight, self.platePos.size()))
+
+        # 显示具有分割位置的车牌的图片：
+        self.splitPosPlate.setPixmap(self.cv2BgrToPixmap(splitPlateImg, self.splitPosPlate.size()))
+
         # 将分割好的字符进行显示
-        #print(characters.shape)
+        # print(characters.shape)
         # print(characters[0].shape)
         try:
             for label in self.labels:
@@ -131,8 +164,10 @@ class MyMainWindow(QMainWindow, Ui_MainWindow):
         # 将分割好的的字符进行识别
         res = ""
         for index, char in enumerate(characters):
-            res += self.char_rec_mode.GetChar(char)
+            res += self.char_rec_mode.GetChar(char, index)
         try:
+            for item in self.reses:
+                item.clear()
             self.res_a.setText(res[0])
             self.res_b.setText(res[1])
             self.res_c.setText(res[2])
@@ -140,7 +175,7 @@ class MyMainWindow(QMainWindow, Ui_MainWindow):
             self.res_e.setText(res[4])
             self.res_f.setText(res[5])
             self.res_g.setText(res[6])
-        except IndexError:
+        except Exception as e:
             pass
         last = ""
         for i in range(len(res)):
@@ -149,3 +184,27 @@ class MyMainWindow(QMainWindow, Ui_MainWindow):
                 last += '·'
         self.result.setText(last)
         pass
+
+
+
+
+
+
+    def toPre(self):
+        # 获取当前所在项的索引，只用到了行row
+        curIdx = self.listView.selectionModel().selectedIndexes()[0]
+        preIdx = self.listView.model().index(curIdx.row() - 1, 0)
+        if preIdx.isValid():
+            self.listView.selectionModel().select(preIdx,
+            QItemSelectionModel.SelectionFlag.Select |
+            QItemSelectionModel.SelectionFlag.SelectCurrent)
+            self.showPicture(preIdx)
+    def toNext(self):
+        curIdx = self.listView.selectionModel().selectedIndexes()[0]
+        nextIdx = self.listView.model().index(curIdx.row() + 1, 0)
+        if nextIdx.isValid():
+            self.listView.selectionModel().select(nextIdx,
+            QItemSelectionModel.SelectionFlag.Select |
+            QItemSelectionModel.SelectionFlag.SelectCurrent)
+            self.showPicture(nextIdx)
+
